5216d4c580863405f0a1e24e5a8eea97bf32e006
KimTaeHan <gms8757@naver.com>
2022-05-03 10:22:51 +0900

    씬 이동 추가 ver_1.2
    Build Setting, Scene Load Function 구현

--- Assets/02.Script/04_GameManager/SceneChanger.cs
+++ Assets/02.Script/04_GameManager/SceneChanger.cs
@@ -8,4 +8,34 @@ public class SceneChanger : Singleton<SceneChanger>
 {
     // TODO
     // 씬관련 코드 작성바람
+
+    // Title 씬로드 함수
+    public void Load0Scene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    // Lobby 씬로드 함수
+    public void Load1Scene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Stage1 씬로드 함수
+    public void Load2Scene()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    // Stage2 씬로드 함수
+    public void Load3Scene()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    // Score 씬로드 함수
+    public void Load4Scene()
+    {
+        SceneManager.LoadScene(4);
+    }
 }
